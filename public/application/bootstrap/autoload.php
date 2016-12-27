3d28cc83a04b8041d07b09140624d6fbbd92b9f0
Oliver Green <green2go@gmail.com>
2016-12-27 22:06:54 +0000

    Removed index.php and moved include path modification into autoload file.

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -1,4 +1,7 @@
 <?php
+
+$vendorPath = realpath(__DIR__ . '/../../../vendor');
+
 /*
  * ----------------------------------------------------------------------------
  * Load all composer autoload items.
@@ -8,8 +11,15 @@
 // If the checker class is already provided, likely we have been included in a separate composer project
 if (!class_exists(\DoctrineXml\Checker::class)) {
     // Otherwise, lets try to load composer ourselves
-    if (!@include(realpath(__DIR__ . '/../../../vendor/autoload.php'))) {
+    if (!@include($vendorPath.'/autoload.php')) {
         echo 'Third party libraries not installed. Ensure that you have installed the dependencies by running the composer install command.';
         die(1);
     }
 }
+
+/*
+ * ----------------------------------------------------------------------------
+ * Add the vendor path to the list of include paths
+ * ----------------------------------------------------------------------------
+ */
+ini_set('include_path', $vendorPath . PATH_SEPARATOR . get_include_path());
