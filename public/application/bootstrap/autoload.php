3519a6c8ba1daf5a7369f27e3c02a01782d516ee
Oliver Green <green2go@gmail.com>
2016-12-27 11:36:51 +0000

    Fixed autoloaders.

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -8,7 +8,7 @@
 // If the checker class is already provided, likely we have been included in a separate composer project
 if (!class_exists(\DoctrineXml\Checker::class)) {
     // Otherwise, lets try to load composer ourselves
-    if (!@include(realpath(__DIR__ . '/../../vendor/autoload.php'))) {
+    if (!@include(realpath(__DIR__ . '/../../../vendor/autoload.php'))) {
         echo 'Third party libraries not installed. Ensure that you have installed the dependencies by running the composer install command.';
         die(1);
     }
--- public/index.php
+++ public/index.php
@@ -1,35 +1,14 @@
 <?php
 /*
  * ----------------------------------------------------------------------------
- * Set our own version of __DIR__ as $__DIR__ so we can include this file on
- * PHP < 5.3 and have it not die wholesale.
- * ----------------------------------------------------------------------------
- */
-$__DIR__ = 'vendor/concrete5/concrete5';
-
-/* ----------------------------------------------------------------------------
- * Override some of the concrete5 dcore directory location
- * ----------------------------------------------------------------------------
- */
-defined('DIRNAME_CORE') or define('DIRNAME_CORE', $__DIR__.'/concrete');
-
-/*
- * ----------------------------------------------------------------------------
  * Add the vendor path to the list of include paths
  * ----------------------------------------------------------------------------
  */
-ini_set('include_path', __DIR__.DIRECTORY_SEPARATOR.'vendor' . PATH_SEPARATOR . get_include_path());
-
-/*
- * ----------------------------------------------------------------------------
- * Include all autoloaders.
- * ----------------------------------------------------------------------------
- */
-require __DIR__ . '/application/bootstrap/autoload.php';
+ini_set('include_path', realpath(__DIR__.'/../vendor') . PATH_SEPARATOR . get_include_path());
 
 /*
  * ----------------------------------------------------------------------------
  * Dispatch the CMS.
  * ----------------------------------------------------------------------------
  */
-$cms = require 'vendor/concrete5/concrete5/concrete/dispatcher.php';
+require 'concrete/dispatcher.php';
